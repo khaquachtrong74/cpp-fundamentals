ba5868556372c7d267aeda519f10682e26bfda91
KhaQT <khaquachtrong74@gmail.com>
2025-04-23 19:08:49 +0700

    Update some library

--- projects/simple-chat-service/include/client.h
+++ projects/simple-chat-service/include/client.h
@@ -1,12 +1,20 @@
 #pragma once
 #include <string>
+#include <iostream>
+#include <array>
+#include <asio.hpp>
+#include <exception>
+#include <fmt/core.h>
+#include <error.h>
 class Client{
     private:
         std::string msg;
         int id;
     public:
         Client(std::string msg, int id);
+        Client();
         ~Client();
         void Connect();
-        void SendMessage();
+        void SendMessage(asio::ip::tcp::socket& socket, std::string message);
+
 };
--- projects/simple-chat-service/include/server.h
+++ projects/simple-chat-service/include/server.h
@@ -1,9 +1,17 @@
 #pragma once
-
+#include <iostream>
+#include <string>
+#include <asio.hpp>
+#include <system_error>
+#include <fstream>
+using asio::ip::tcp;
 class Server{
     public:
-        static void GetMessage();
+        static void WriteLog(std::array<char, 128> buf, size_t len);  
+        static void GetMessage(std::error_code& error, tcp::socket& socket,std::array<char, 128>&buf );
+        static void SendMessage(asio::ip::tcp::socket& socket,std::string message);        
         static void SendNotification();
         static void HandleClientConnection();
-
+        static void Resolve(std::string dmn, std::string port);
+        static void TurnOnServer();
 };
